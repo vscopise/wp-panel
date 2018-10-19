2915360e4dc9bcc742689e831ec382c53c5d60d3
Vicente Scópise <vscopise@gmail.com>
2018-10-18 23:52:52 -0300

    Tercer commit

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
 
-
-
 import { 
+  Checkbox,
   createMuiTheme,
   List,
   ListItem,
   ListItemText,
   MuiThemeProvider,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  TableSortLabel,
+  Tooltip,
   Typography 
 } from '@material-ui/core'
 
 import * as constants from '../includes/constants'
 
-const theme = createMuiTheme ({
-  typography: {
-      useNextVariants: true,
-  }
+const styles = theme => ({
+    table: {
+      minWidth: 700
+    },
+    td_id: {
+      minWidth:'10%'
+    },
+    td_tit: {
+      minWidth:'20%'
+    }
 })
 
 class Home extends Component {
@@ -28,6 +41,7 @@ class Home extends Component {
       isLoading: true
     } 
   }
+
 
 
   componentDidMount(){
@@ -45,24 +59,35 @@ class Home extends Component {
     })    
   }
 
-  render() {
+  onSelectAllClick = () => {}
+
+  render(props) {
+    const classes = {props}
+
     return (
-      
-
-
-      <MuiThemeProvider theme={theme}>
-        <List component="nav">
-          {this.state.posts.map((post) => 
-            <ListItem
-              button
-              
-            >
-              <ListItemText primary={post.title.rendered} />
-
-            </ListItem>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell numeric>Id</TableCell>
+            <TableCell className={classes.td_tit}>Título</TableCell>
+            <TableCell padding={'dense'}>Categorías</TableCell>
+            <TableCell padding={'dense'}>Etiquetas</TableCell>
+            <TableCell padding={'dense'}>Fecha</TableCell>
+            
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        {this.state.posts.map((post) => 
+            <TableRow hover={true} style={{ height: 48 }}>
+              <TableCell numeric>{post.id}</TableCell>
+              <TableCell padding={'dense'}>{post.title.rendered}</TableCell>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+              <TableCell>{post.date}</TableCell>
+            </TableRow>
           )}
-        </List>
-      </MuiThemeProvider>
+        </TableBody>
+      </Table>
     );
   }
 }
